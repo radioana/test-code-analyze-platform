b6e76d1ec33bc3fa9da5c80e9a5795670f7e1e03
Andrei Husar <ahusar@pitechnologies.ro>
2015-05-11 12:00:30 +0300

    Add 2 test files

---
+++ CodeSizeTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Acme\DemoBundle\Controller;
+
+class CodeSizeTest
+{
+    public function example($a, $b , $c, $d, $e, $f, $h, $a2, $b2, $a1, $b1, $z)
+    {
+        if ($a == $b) {
+            if ($a1 == $b1) {
+                fiddle();
+            } else if ($a2 == $b2) {
+                fiddle();
+            } else {
+                fiddle();
+            }
+        } else if ($c == $d) {
+            while ($c == $d) {
+                fiddle();
+            }
+        } else if ($e == $f) {
+            for ($n = 0; $n < $h; $n++) {
+                fiddle();
+            }
+        } else {
+            switch ($z) {
+                case 1:
+                    fiddle();
+                    break;
+                case 2:
+                    fiddle();
+                    break;
+                case 3:
+                    fiddle();
+                    break;
+                default:
+                    fiddle();
+                    break;
+            }
+        }
+    }
+}
