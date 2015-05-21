b4c0fb882d995a052d12bb0c6501a9f7f172c290
Andrei Husar <ahusar@pitechnologies.ro>
2015-05-21 18:01:05 +0300

    asdasd

--- SecuredController.php
+++ SecuredController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Test123123;
+namespace Test1231231111;
 namespace Acme\DemoBundle\Controller\MyTest\Secondest\NewPR\TestDate\RemoveFromArray;
 
 use Symfony\Component\HttpFoundation\Request;
